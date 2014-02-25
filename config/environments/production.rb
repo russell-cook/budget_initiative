d58281248d61208b063f82e7c5d9c90831393a20
russell-cook <russ.cook@gmail.com>
2014-02-25 10:56:10 -0800

    configure sendgrid + devise initializer for heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ BudgetInitiative::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { :host => 'ppbb-demo.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
