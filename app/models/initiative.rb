e1cd1f93ee262995fae05fd441bbafe0318c36e2
Russell Cook <russ.cook@gmail.com>
2014-03-18 22:38:13 -0700

    add validation presence: true to initiative title and lead agency

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -2,5 +2,7 @@ class Initiative < ActiveRecord::Base
 	belongs_to :user
 	has_many :shares
 	validates :user_id, presence: true
+  validates :title, presence: true
+  validates :lead_agency, presence: true
 	accepts_nested_attributes_for :shares
 end
