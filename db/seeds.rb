c131f12a4f055bc566e5c6e47b82c20ef252ee8a
russell-cook <russ.cook@gmail.com>
2014-04-17 15:51:44 -0700

    add analyst admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = Admin.create(email: 'admin@admin.nv.gov', password: 'Skoofer_78', password_confirmation: 'Skoofer_78')
+# admin = Admin.create(email: 'admin@admin.nv.gov', password: 'Skoofer_78', password_confirmation: 'Skoofer_78')
+analyst_admin = Admin.create(email: 'budget@admin.nv.gov', password: 'budget2014', password_confirmation: 'budget2014')
